6f44048c19cb821e6004cd68d9475e3e8cfc2698
Raul Rodrigues <rauliporto@sapo.pt>
2021-11-10 22:50:43 +0000

    1st

---
+++ src/pt/iscte/pcd/storage_nodes/ErrorInject.java
@@ -0,0 +1,9 @@
+package pt.iscte.pcd.storage_nodes;
+
+public class ErrorInject extends Thread {
+
+    @Override
+    public void run() {
+
+    }
+}
--- src/pt/iscte/pcd/storage_nodes/StorageNode.java
+++ src/pt/iscte/pcd/storage_nodes/StorageNode.java
@@ -22,7 +22,7 @@ public class StorageNode {
     private ConsoleTracker tracker; //Uma thread para monotorizar se são injetados erros na consola
     private ByteErrorChecker checker; //Duas threads que vão estar a percorrer os bytes do node para verificação de erros
 
-    private String network; //localhost
+    private String nodeAdress; //localhost
     private String directoryPort; //8080
     private String nodePort; //8081, 8082, etc.
 
@@ -34,68 +34,64 @@ public class StorageNode {
     private CloudByte[] cloudArrayFile;
 
     //Construtor - se argumento diferente de nullo converte para cloudbytes, caso contrario ....
-    public StorageNode(String fileName) throws IOException {
-        //
-
+    public StorageNode(String directoryAdress, String directoryPort, String nodePort, String fileName) throws IOException {
+        this.nodeAdress = directoryAdress;
+        this.directoryPort = directoryPort;
+        this.nodePort = nodePort;
         if (fileName != null)
             convertToCloudBytes(fileName);
+    }
 
-
-        // TODO Auto-generated constructor stub
+    public StorageNode(String nodeAdress, String directoryPort, String nodePort) {
+        this.nodeAdress = nodeAdress;
+        this.directoryPort = directoryPort;
+        this.nodePort = nodePort;
     }
 
 
     private void convertToCloudBytes(String fileName) throws IOException {
         byte[] fileArray = Files.readAllBytes(Paths.get(fileName));
+        cloudArrayFile = new CloudByte[fileArray.length];
         for (int i = 0; i < fileArray.length; i++) {
             cloudArrayFile[i] = new CloudByte(fileArray[i]);
         }
     }
 
+    // Criaçao de socket para contacto com servidor
     private void connect() throws IOException {
-        InetAddress url = InetAddress.getByName(network);
-        socket = new Socket(url, Integer.parseInt(nodePort));
-
+        InetAddress url = InetAddress.getByName(nodeAdress);
+        socket = new Socket(url, Integer.parseInt(directoryPort));
+        in = new BufferedReader(new InputStreamReader(socket.getInputStream()));
+        out = new PrintWriter(new BufferedWriter(new OutputStreamWriter(socket.getOutputStream())), true);
     }
-
+    // Finalização da socket de contacto com servidor
     private void disconnect() throws IOException {
         socket.close();
-
     }
 
-
-    //registar nó no diretorio
+    // Registar nó no diretorio
     public void register() throws IOException {
         String register_string = "INSC 127.0.0.1 " + nodePort;
         System.out.println(register_string);
-        PrintWriter register = new PrintWriter(new BufferedWriter(new OutputStreamWriter(socket.getOutputStream())), true);
-        register.println(register_string);
-
-        BufferedReader in = new BufferedReader(new InputStreamReader(
-                socket.getInputStream()));
+        out.println(register_string);
         String str = in.readLine();
         System.out.println(str);
-        register.println("end");
-
-
+ // Está a faltar algo pois fica preso aqui o codigo - perguntei como funciona o diretorio para perceber o que é necessário
     }
 
 
-    // obter a lista de todos os N�s atrav�s da solicita��o ao diretorio
+    // obter a lista de todos os Nós atraves da solicitacao ao diretorio - ESBOÇO
+    // Questionei o que responde o diretorio, se um array ou string por cada NÓ
     public String[] getNodesList() throws IOException {
-
         connect();
         String[] list = new String[10];
         int i = 0;
-
         while (true) {
-
             if (("valor" == "end") || ("valor" == "null"))
                 break;
             else
                 list[i] = "valor";
         }
-
         disconnect();
         return list;
 
@@ -115,10 +111,16 @@ public class StorageNode {
 
     }
 
-    public static void main(String[] args) throws IOException {
+    // Fase 3 - Mecanismos de injeção de erros
 
-        StorageNode novo_no = new StorageNode(args[1]);
+
+
+    public static void main(String[] args) throws IOException {
+        System.out.println(args[0]);
+        StorageNode novo_no = new StorageNode("127.0.0.1", "8080", "8081", "data.bin");
+        novo_no.connect();
         novo_no.register();
+        novo_no.disconnect();
 
     }
 
