a968acdca24b7388f3ce3b96f4102384f5c3e8e3
rauliporto <79728100+rauliporto@users.noreply.github.com>
2021-11-16 21:55:21 +0000

    2nd

--- src/pt/iscte/pcd/client/Client.java
+++ src/pt/iscte/pcd/client/Client.java
@@ -16,10 +16,14 @@ import javax.swing.WindowConstants;
 public class Client {
 	
 	private JFrame frame;
+	private String nodeAdress;
+	private String nodePort;
+
 	
-	public Client() {
-		
+	public Client(String nodeAdress, String nodePort) {
 		this.frame = new JFrame("Client");
+		this.nodePort = nodePort;
+		this.nodeAdress = nodeAdress;
 		frame.setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
 		addFrameContent();
 		frame.pack();
@@ -52,12 +56,21 @@ public class Client {
 		frame.add(textFieldComprimento);
 		frame.add(buttonConsultar);
 		frame.add(textAreaRespostas);
-		
+	}
+
+	private void requestBytes(int size, int position){
+		InetAddress url = InetAddress.getByName(nodeAdress);
+		socket = new Socket(url, Integer.parseInt(directoryPort));
+		in = new BufferedReader(new InputStreamReader(socket.getInputStream()));
+		out String register_string = "REQ " + nodePort;
+		System.out.println(register_string);
+		out.println(register_string); = new PrintWriter(new BufferedWriter(new OutputStreamWriter(socket.getOutputStream())), true);
+
 	}
 
 	public static void main(String[] args) {
 		
-		Client frame = new Client();
+		Client frame = new Client(args[0],args[1]);
 		
 	}
 
